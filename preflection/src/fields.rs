d90bc830f602e19e9afd65651b6d3b2ae56c67a2
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-03-13 13:29:01 -0400

    Creates an initial version of HasFields

---
+++ preflection/src/fields.rs
@@ -0,0 +1,68 @@
+mod errors;
+
+pub use self::errors::FieldAccessError;
+use core::any::Any;
+
+type FieldAccessResult<T> = Result<T, FieldAccessError>;
+
+/// A trait that can be used to dynamically access the fields of a struct at
+/// runtime.
+pub trait HasFields {
+    fn get_field_raw<'s>(&'s self, name: &str) -> FieldAccessResult<&'s dyn Any>;
+    fn get_field_mut_raw<'s>(&'s mut self, name: &str) -> FieldAccessResult<&'s mut dyn Any>;
+}
+
+/// A trait that provides useful extension methods that make dynamically
+/// accessing struct fields at runtime easier.
+pub trait HasFieldsExt {
+    fn get_field<'s, T: 'static>(&'s self, name: &str) -> FieldAccessResult<&'s T>;
+    fn get_field_mut<'s, T: 'static>(&'s mut self, name: &str) -> FieldAccessResult<&'s mut T>;
+}
+
+impl<T: HasFields> HasFieldsExt for T {
+    fn get_field<'s, U: 'static>(&'s self, name: &str) -> FieldAccessResult<&'s U> {
+        self.get_field_raw(name)?
+            .downcast_ref::<U>()
+            .ok_or(FieldAccessError::InvalidType)
+    }
+
+    fn get_field_mut<'s, U: 'static>(&'s mut self, name: &str) -> FieldAccessResult<&'s mut U> {
+        self.get_field_mut_raw(name)?
+            .downcast_mut::<U>()
+            .ok_or(FieldAccessError::InvalidType)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    struct User {
+        id: u32,
+    }
+
+    impl HasFields for User {
+        fn get_field_raw<'s>(&'s self, name: &str) -> FieldAccessResult<&'s dyn Any> {
+            match name {
+                "id" => Ok(&self.id),
+                _ => Err(FieldAccessError::MissingField),
+            }
+        }
+        fn get_field_mut_raw<'s>(&'s mut self, name: &str) -> FieldAccessResult<&'s mut dyn Any> {
+            match name {
+                "id" => Ok(&mut self.id),
+                _ => Err(FieldAccessError::MissingField),
+            }
+        }
+    }
+
+    #[test]
+    fn get_field_test() {
+        let user = User { id: 1 };
+
+        let id = user.get_field::<u32>("id").unwrap();
+
+        assert_eq!(id, &user.id);
+    }
+}
---
+++ preflection/src/fields/errors.rs
@@ -0,0 +1,23 @@
+use core::fmt::Display;
+use core::fmt::Formatter;
+use core::fmt::Result as FmtResult;
+
+/// An error that occurred while accessing a field through the preflection API.
+#[derive(Debug)]
+pub enum FieldAccessError {
+    MissingField,
+    InvalidType,
+}
+
+impl Display for FieldAccessError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            FieldAccessError::MissingField => {
+                write!(f, "The specified field did not exist for the object.")
+            }
+            FieldAccessError::InvalidType => {
+                write!(f, "The specified field is of a different type.")
+            }
+        }
+    }
+}
